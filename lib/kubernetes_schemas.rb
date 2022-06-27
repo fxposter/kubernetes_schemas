0a504629652ac2ad54fa667410023eb50387c9be
Pavel Forkert <fxposter@gmail.com>
2022-06-27 13:56:06 +0300

    initial commit

---
+++ lib/kubernetes_schemas.rb
@@ -0,0 +1,6 @@
+require "kubernetes_schemas/version"
+
+module KubernetesSchemas
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/kubernetes_schemas/version.rb
@@ -0,0 +1,3 @@
+module KubernetesSchemas
+  VERSION = "0.1.0"
+end
